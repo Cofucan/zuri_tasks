886f2a59da896f28e0b4bff8b4979cc56ff4f184
Cofucan <coolucheofia@gmail.com>
2022-06-09 19:29:19 +0100

    removed commented code at the end

--- Classes-and-Objects/main.py
+++ Classes-and-Objects/main.py
@@ -1,6 +1,6 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self, name, age:int, tracks:list, score):
+    def __init__(self, name, age: int, tracks: list, score):
         assert type(age) == int, "Age must be an integer"
         assert type(tracks) == list or tuple, "Tracks should be a list with at least one item"
         assert type(score) == float or int, "Score should be a number"
@@ -9,22 +9,21 @@ class Student:
         self.age = age
         self.tracks = tracks
         self.score = score
-    
+
     def change_name(self, new_name):
         self.name = new_name
-    
+
     def change_age(self, new_age):
         self.age = new_age
 
     def add_track(self, track):
         self.tracks.append(track)
-    
+
     def get_score(self):
         print(f"The score of {self.name} is {self.score}.")
 
 
-
-Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+Bob = Student(name="Bob", age=26, tracks=["FE", "BE"], score=20.90)
 
 # Expected methods
 Bob.change_name("Peter")
@@ -33,40 +32,3 @@ Bob.add_track("UI/UX")
 Bob.get_score()
 
 print(Bob.tracks)
-
-
-
-
-
-
-# class Student:
-#     # [assignment] Skeleton class. Add your code here
-#     def __init__(self, name, age:int, tracks, score):
-#         self.name = name
-#         self.age = age
-#         self.tracks = tracks
-#         self.score = score
-
-#     def change_name(self, n_name):
-#         self.name = n_name
-    
-#     def change_age(self, n_age):
-#         self.age = n_age
-
-#     def add_track(self, n_track):
-#         self.tracks.append(n_track)
-    
-#     def get_score(self):
-#         print(f"{self.name}'s score is {self.score}.")
-
-
-
-# Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
-
-# # Expected methods
-# Bob.change_name("Peter")
-# Bob.change_age(34)
-# Bob.add_track("UI/UX")
-# Bob.get_score()
-
-# print(Bob.tracks)
