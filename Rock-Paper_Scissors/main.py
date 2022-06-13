405c095356775c09253df793f6aa4cb469aadcf9
Cofucan <coolucheofia@gmail.com>
2022-06-13 03:38:01 +0100

    updated rpc file

--- Rock-Paper_Scissors/main.py
+++ Rock-Paper_Scissors/main.py
@@ -8,7 +8,7 @@ record = {'r': 'rock', 'p': 'paper', 's': 'scissors'}
 
 print('Welcome to our game of Rock-Paper-Scissors')
 
-user_win = 0
+player_win = 0
 cpu_win = 0
 
 while True:
@@ -16,13 +16,13 @@ while True:
         print('Pls enter one of the 3 options below')
         for item in outcomes:
             print(item)
-        user_choice = input('Enter the letter or the word: ').lower()
+        player_choice = input('Enter the letter or the word: ').lower()
 
-        is_letter = user_choice in options_l
-        is_word = user_choice in options_w
+        is_letter = player_choice in options_l
+        is_word = player_choice in options_w
 
         if is_letter:
-            user_choice = record[user_choice]
+            player_choice = record[player_choice]
             break
         elif is_word:
             break
@@ -32,26 +32,26 @@ while True:
 
     cpu_choice = random.choice(options_w)
 
-    print(f'Player {user_choice} : CPU {cpu_choice}')
+    print(f'Player {player_choice} : CPU {cpu_choice}')
 
-    if user_choice == ('rock') and cpu_choice == ('paper'):
-        print('Sorry! You lost')
+    if player_choice == ('rock') and cpu_choice == ('paper'):
+        print('Sorry! You lost \n')
         cpu_win += 1
-    elif user_choice == ('paper') and cpu_choice == ('rock'):
-        print('Congrats! You won')
-        user_win += 1
-    elif user_choice == ('rock') and cpu_choice == ('scissors'):
-        print('Congrats! You won')
-        user_win += 1
-    elif user_choice == ('scissors') and cpu_choice == ('rock'):
-        print('Sorry! You lost')
+    elif player_choice == ('paper') and cpu_choice == ('rock'):
+        print('Congrats! You won \n')
+        player_win += 1
+    elif player_choice == ('rock') and cpu_choice == ('scissors'):
+        print('Congrats! You won \n')
+        player_win += 1
+    elif player_choice == ('scissors') and cpu_choice == ('rock'):
+        print('Sorry! You lost \n')
         cpu_win += 1
-    elif user_choice == ('paper') and cpu_choice == ('scissors'):
-        print('Sorry! You lost')
+    elif player_choice == ('paper') and cpu_choice == ('scissors'):
+        print('Sorry! You lost \n')
         cpu_win += 1
-    elif user_choice == ('scissors') and cpu_choice == ('paper'):
-        print('Congrats! You won')
-        user_win += 1
+    elif player_choice == ('scissors') and cpu_choice == ('paper'):
+        print('Congrats! You won \n')
+        player_win += 1
     else:
         print('Ok that was a tie so we have go again.')
         continue
@@ -59,13 +59,13 @@ while True:
     try_again = input('Do you want to try again? \n y / n \n')
 
     if try_again == 'n':
-        print('Here are the scores')
-        print(f'You : {user_win}')
+        print('\nHere are the scores')
+        print(f'You : {player_win}')
         print(f'CPU : {cpu_win}')
 
-        if user_win > cpu_win:
+        if player_win > cpu_win:
             print('You win the game!')
-        elif user_win < cpu_win:
+        elif player_win < cpu_win:
             print('CPU wins the game')
         else:
             print('The game was a tie')
