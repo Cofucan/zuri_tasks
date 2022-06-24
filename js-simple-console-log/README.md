c92d20708698b31239ad67e6076051c8fbd7b4b0
Cofucan <coolucheofia@gmail.com>
2022-06-24 15:39:26 +0100

    updated console alert

---
+++ js-simple-console-log/README.md
@@ -0,0 +1,3 @@
+# Here is the link to the hosted GitHub page
+
+https://cofucan.github.io/javascript-about-me/
--- js-simple-console-log/simple-console-log.js
+++ js-simple-console-log/simple-console-log.js
@@ -1,5 +1,9 @@
-console.log("My name is Uche Ofia");
-console.log("I am 182cm tall");
-console.log("I am from Nigeria");
+let myName = "My name is Uche Ofia"
+let myHeight = "I am 182cm tall"
+let myCountry = "I am from Nigeria"
+
+console.log(myName);
+console.log(myHeight);
+console.log(myCountry);
 
 alert('My name is Uche Ofia. I am 182cm tall and I am from Nigeria.')
